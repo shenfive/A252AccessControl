afec388db512211a780f0d1ce263d0d4e33a3c8a
Danny Shen <shenfive@gmail.com>
2025-04-20 10:21:40 +0800

    Initial Commit

---
+++ A252AccessControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A252AccessControl
+//
+//  Created by 申潤五 on 2025/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
